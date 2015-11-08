d15e840f197b4807ed89043a1308e01e52e343dd
toni <toni@pc1400>
2015-11-08 21:59:04 +0100

    remove the lisp interface to lightningfn because it depends on AUTOWRAP and this is not supported on some lisps, like CLISP. I will write an interface using CFFI.

--- library.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :lightningfn)
-
-(cffi:define-foreign-library liblightningfn
-  (:unix (:or "lightningfn.so" "/home/toni/soft/lightningfn/lightningfn.so"))
-  (t (:default "lightningfn")))
-
-(cffi:use-foreign-library liblightningfn)
--- spec/lightningfn.h
+++
@@ -1,1 +0,0 @@
-../lightningfn.h
